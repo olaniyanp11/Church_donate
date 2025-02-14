9cb6ad51a9b779f455b508f5bd83edf59f14ea22
olaniyanp11 <olaniyanp11@.com>
2025-02-14 21:09:06 +0100

    ui redesign

--- src/components/AboutSec/AboutSec.jsx
+++ src/components/AboutSec/AboutSec.jsx
@@ -8,42 +8,43 @@ export const AboutSec = () => {
     return (
         <section className='bg-secondary w-full gap-4 pb-10'>
             {/* Animated title */}
-            <motion.h2
-                className='text-[30px] font-bold text-primary text-center py-7'
-                whileInView={{ opacity: 1, y: 0 }}
-                initial={{ opacity: 0, y: -50 }}
-                viewport={{ once: true }}
-                transition={{ duration: 1 }}
-            >
-                About the Initiative
-            </motion.h2>
+           
 
             <div className='flex flex-col sm:flex-row w-full items-center md:px-[100px] gap-8'>
                 {/* Animated text section */}
                 <motion.div
-                    className="flex flex-col px-5 items-start sm:w-1/2 gap-5"
+                    className="flex flex-col px-5 items-start sm:w-1/2 gap-5 pt-24"
                     whileInView={{ opacity: 1, x: 0 }}
                     initial={{ opacity: 0, x: -100 }}
                     viewport={{ once: true }}
                     transition={{ duration: 1, delay: 0.2 }}
                 >
-                    <p className='tracking-wider md:text-[18px] xl:text-[20px]'>
-                        The ISM Ministers’ Support Program is dedicated to providing ministers of the gospel in dire need worldwide with essential support, including food relief, musical instruments, and multimedia equipment, along with a one-month ISM Online School training to equip them for impactful ministry. With your support, we can provide critical resources, training, and financial aid to help these ministers overcome their challenges and continue their divine calling effectively.
+                    <motion.h2
+                        className='text-[30px] font-bold text-primary text-center pt-2 border-l-4 border border-x-0 border-y-0 px-5 border-primary'
+                        whileInView={{ opacity: 1, y: 0 }}
+                        initial={{ opacity: 0, y: -50 }}
+                        viewport={{ once: true }}
+                        transition={{ duration: 1 }}
+                    >
+                        About the Initiative
+                    </motion.h2>
+                    <p className='tracking-normal md:text-[16px] xl:text-[19px]'>
+                        The ISM Minister's support Program is dedicated to providing ministers of the gospel in dire need worldwide with essential support, including food relief, musical instruments, and multimedia equipment, along with a one-month ISM Online School training to equip them for impactful ministry. With your support, we can provide critical resources, training, and financial aid to help these ministers overcome their challenges and continue their divine calling effectively.
                     </p>
-                    <Link to='/about' className="bg-accent p-2 text-white px-10 text-center w-[250px]">
+                    <Link to='/about' className="bg-accent p-2 text-white px-10 text-center w-[250px] rounded-md">
                         Learn more
                     </Link>
                 </motion.div>
 
                 {/* Animated image */}
                 <motion.div
-                    className='px-5 sm:w-1/2'
+                    className='px-5 sm:w-1/2 rounded-sm'
                     whileInView={{ opacity: 1, scale: 1 }}
                     initial={{ opacity: 0, scale: 0.8 }}
                     viewport={{ once: true }}
                     transition={{ duration: 1, delay: 0.4 }}
                 >
-                    <img src={About} alt="About the initiative" className='w-full' />
+                    <img src={About} alt="About the initiative" className='w-full rounded-md' />
                 </motion.div>
             </div>
         </section>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { FaDrum, FaUtensils, FaMicrophone } from "react-icons/fa";
+import Food from "../assets/two.jpg";
+import Music from "../assets/five.jpg";
+import Custom from "../assets/seven.jpg";
+import Speak from "../assets/mixer.jpg";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 
+const fadeInUp = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
+};
+
 const Categories = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const categories = [
         {
             title: "Category A : Food Relief",
@@ -12,6 +21,7 @@ const Categories = () => {
             description:
                 "We provide ministers with food items to support them and their families for one month. Each package is valued at $150 and offers essential sustenance to ministers working in challenging conditions. Every minister supported in this category also receives access to a one-month foundational training program at the ISM Online School.",
             cta: "Support A Minister with Food Relief Today!",
+            image:Food
         },
         {
             title: "Category B : Musical Instruments",
@@ -19,6 +29,7 @@ const Categories = () => {
             description:
                 "Music plays a vital role in worship and ministry. In this category, we provide ministers with essential musical equipment, including drum sets and keyboards, valued at $1,000. Ministers in this category will also receive a one-month foundational training program at the ISM Online School to enhance their ministry effectiveness.",
             cta: "Support A Minister with Musical Instruments Today!",
+            image: Music,
         },
         {
             title: "Category C : Custom Support",
@@ -26,6 +37,7 @@ const Categories = () => {
             description:
                 "Every minister's need is unique. While some require food relief, musical instruments, or multimedia equipment, others may have specific challenges that demand personalized support.With the Custom Support Category, you have the opportunity to sponsor a minister in a way that directly addresses their most urgent and specific needs—whether it’s helping with church infrastructure, ministry resources, transportation, or any other essential requirement that empowers them to continue their work in the Gospel.",
             cta: "Support A Minister with Multimedia and Sound!",
+            image: Custom
         },
         {
             title: "Category D : Multimedia and Sound Systems",
@@ -33,33 +45,74 @@ const Categories = () => {
             description:
                 "Modern ministry often relies on effective communication tools. This category provides ministers with multimedia and sound equipment, including microphones, speakers, projectors, and projector screens. Ministers supported here will also gain access to a one-month foundational training program at the ISM Online School, helping them maximize the impact of their ministry tools.",
             cta: "Support A Ministers Unique need",
+            image: Speak
         },
     ];
 
     return (
-        <div className="bg-gray-100 py-10 px-4 md:px-12 lg:px-10">
-            <h2 className="text-3xl font-bold text-center mb-8 text-accent">Categories of Support</h2>
-            <p className="text-center text-gray-600 mb-12">
-                We've designed our support to meet specific needs, categorized into three impactful areas. Explore the categories below and see how you can make a difference:
-            </p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 px-5 sm:px-10">
-                {categories.map((category, index) => (
-                    <motion.div
-                        key={index}
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.95 }}
-                        className="bg-white shadow-md rounded-lg p-6 text-left flex flex-col justify-between "
-                    >
-                        {/* <div className="mb-4">{category.icon}</div> */}
-                        <h3 className="text-xl font-bold mb-4 text-accent ">{category.title}</h3>
-                        <p className="text-gray-600 mb-6 text-md">{category.description}</p>
-                        <button className="bg-accent text-white py-2 px-4 rounded hover:bg-[#26688f] " onClick={() => navigate('/refer-minister')}>
-                            {category.cta}
-                        </button>
-                    </motion.div>
-                ))}
+        <>
+            <div className="bg-gray-100 pt-10 px-4 md:px-12 lg:px-10 flex-col flex justify-center items-center">
+                <motion.h2
+                    initial="hidden"
+                    animate="visible"
+                    variants={fadeInUp}
+                    className="text-3xl font-bold text-center mb-2 text-accent"
+                >
+                    Categories of Support
+                </motion.h2>
+                <motion.p
+                    initial="hidden"
+                    animate="visible"
+                    variants={fadeInUp}
+                    className="text-center text-gray-600 mb-12 w-[80%]"
+                >
+                    We've designed our support to meet specific needs, categorized into four impactful areas. Explore the categories below and see how you can make a difference:
+                </motion.p>
             </div>
-        </div>
+
+            {categories.map((category, index) => (
+                <section
+                    key={index}
+                    className={`sm:px-20 px-4 py-8 ${index % 2 !== 0 ? "bg-accent text-white" : ""}`}
+                >
+                    <motion.div
+                     
+                        
+                        className={`sm:flex-row${index % 2 !== 0 ? "-reverse" : ""} flex justify-center relative w-full gap-10 items-center flex-col`}
+                    >
+                        <motion.div
+                        whileInView={{ opacity: 1, scale: 1 }}
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        viewport={{ once: true }}
+                        transition={{ duration: 1, delay: 0.4 }}
+                            className="sm:w-1/2 w-full py-5">
+                            <img src={category.image} alt={category.title} className="sm:w-[90%] w-full rounded-md" />
+                        </motion.div>
+                        <motion.div
+                            whileInView={{ opacity: 1, y: 0 }}
+                            initial={{ opacity: 0, y: -50 }}
+                            viewport={{ once: true }}
+                            transition={{ duration: 1 }}
+                            className="sm:w-1/2 w-full text-lg tracking-normal">
+                            <h1 className={`font-bold text-2xl mb-2 px-4 border-l-4 ${index % 2 !== 0 ? "border-l-white" : "border-l-accent text-accent"}`}>
+                                {category.title}
+                            </h1>
+                            {category.description}
+                            <br />
+                            <button
+                                onClick={() => navigate("/refer-minister")}
+                                className={`px-8 py-2 rounded-md mt-3 transition-all duration-300 ${index % 2 !== 0
+                                        ? "bg-white text-accent font-bold hover:bg-accent hover:text-white hover:border"
+                                        : "bg-accent text-white"
+                                    }`}
+                            >
+                                {category.cta}
+                            </button>
+                        </motion.div>
+                    </motion.div>
+                </section>
+            ))}
+        </>
     );
 };
 
